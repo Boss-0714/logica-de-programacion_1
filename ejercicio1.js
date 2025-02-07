26511f7e487de0036468f55ad0b7c65d8734e85b
Diego Cubillos <alejandroibanez035@gmail.com>
2025-02-06 23:11:01 -0500

    Update ejercicio1.js

--- ejercicio1.js
+++ ejercicio1.js
@@ -4,30 +4,15 @@
 //pasar un argumento a la funcion 
 //comparar el parametro con el parametro al revez
 
-// function palindromo(str){
+ function palindromo(str){
 
-//     return str === str.split("").reverse().join("")
+     return str === str.split("").reverse().join("")
 
-// }
+ }
 
-// console.log(palindromo('radar'));
+ console.log(palindromo('radar'));
+//solo busque la solucion 
 
 
 
 
-//iterables y iterators
-//cuando un tipo de dato es iterable significa que es una estrutura de datos lineal que hace sus elementos sean publicos y se puedan recorrer
-
-const iterable = [1,2,3,4,5];
-
-//acedemos al iterador del iterable
-const iterador = iterable[Symbol.iterator]();
-
-console.log(Symbol.iterator)
-
-console.log(iterable);
-console.log(iterador);
-
-//nos devuelve un objeto
-console.log(iterador.next());
-//el value nos dice el elemento y done si quedan o no elementos 
